772e6178e68a9c98c94955205144ce753098d17b
Tahir Mbaye <mbayepro6@gmail.com>
2022-05-13 23:09:06 +0000

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'mbayepro/inscription_poo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mbayepro/inscription_poo' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
